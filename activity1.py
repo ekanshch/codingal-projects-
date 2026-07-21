fbd7861a033c98ba2db5a49f2c25a8f05fea7ada
ekanshch <ekansh0506@gmail.com>
2026-07-21 16:39:17 +0530

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,8 @@
+# Printing a greeting message 
+print("Hello, World!")
+#Printing my name 
+print("My name is Ekansh.")
+#printing a fun fact about myself 
+print("I love soccer.")
+#performing and printing a simple calculation 
+print("The result of 3+5 is:", 3+5)
