d9885ffa6ba8bddaaea19fa57d124c69b7587532
ekanshch <ekansh0506@gmail.com>
2026-07-24 13:41:58 +0530

    Add files via upload

---
+++ mydailyroutine.py
@@ -0,0 +1,17 @@
+# Printing a greeting message 
+print("Hello, Today, I will be sharing my daily routine.")
+print("I start my day by waking up at 5:30 AM.\n")
+# Printing my routine for morning 
+print("After waking up, I brush my teeth and take a bath.")
+print("Then, I get ready for school and have my breakfast which usually consists of milk.")
+print ("I leave for school at 6:15 AM as my bus arrives till 6:30 AM.")
+print("I reach school by 7:00 AM. and continue my classes until 2:30 PM. \n")
+# printing my routine after school
+print ("After school, I return home and have my lunch till 3:00 PM.")
+print("After lunch, I take a short nap and then leave my home for my coaching classes at around 4:00 PM.")
+print ("My coaching classes go on till 8:30 PM. After that, I return and have my dinner till 9:00 PM.\n")
+print("After dinner, I go to play with my friends or watch TV for some time. ")
+#the time when i go to bed 
+print("I go to bed at around 10:15 PM. and sleep till 5:30 AM the next day.")
+#calculating my study time for a week
+print("\n I study for 1.5 hours on week days and 2 hours on weekends. So, in a week I study for a total of ", (1.5*5)+(2*2), "hours.")
