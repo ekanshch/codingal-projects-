b36b727a9ed7e1b4f24fe79410ed613e289581a0
Ekansh <ekansh0506@gmail.com>
2026-09-12 09:59:51 +0530

    Add files via upload

---
+++ musicplaylistmanager.py
@@ -0,0 +1,12 @@
+playlist = ["Shape of You", "Blinding Lights", "Levitating", "Peaches", "Save Your Tears"]
+
+print("======= MUSIC PLAYLIST =======")
+
+print("\n Total Songs :-"   , len(playlist))
+
+print("First Song :-"  , playlist[0])
+print("Last Song :-"  , playlist[-1])
+
+print("Top Three Songs :-" "\n" , playlist[0:3])
+
+print("Songs in Reverse Order :-" "\n" , playlist[::-1])
