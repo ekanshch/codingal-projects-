51bdb502dbb4a7bb32b77667363d4019b710e349
Ekansh <ekansh0506@gmail.com>
2026-09-12 10:04:32 +0530

    Add files via upload

---
+++ videogamecollectionmanager.py
@@ -0,0 +1,10 @@
+games = ["Minecraft", "Roblox", "Fortnite", "Among Us", "Super Mario Odyssey", "Call of Duty: Warzone", "Apex Legends", "Grand Theft Auto V"]
+
+print("=========VIDEO GAME COLLECTION =========")
+
+print("Total games:", len(games))
+print("\nFirst game:", games[0])
+print("Last game:", games[-1])
+print("\nTop three games:\n", games[0:3])
+print("\nGames in reverse order:\n", games[::-1])   
+
